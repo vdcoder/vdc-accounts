712d8ecd504ee6bb01a246611061fb37d4b77f5b
Victor Diaz <vdcoder@gmail.com>
2025-09-28 15:44:31 -0400

    Adjust database connection string for Railway compatibility

--- packages/nextjs/lib/db.ts
+++ packages/nextjs/lib/db.ts
@@ -5,7 +5,7 @@ import { Pool } from 'pg';
 const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Set this in your .env file
+  connectionString: process.env.DATABASE_URL?.replaceAll('/railway', '/postgres'), // Adjust for Railway
   // Only use SSL in production
   ...(isProduction ? { ssl: { rejectUnauthorized: false } } : {}),
 });
